bb34858eb76112adf112ad48abf871ce4a1bca52
Robin Pedersen <robin.pedersen@visma.com>
2023-12-02 15:31:38 +0100

    Extract lists of resources for easy selection

--- src/extract.rs
+++ src/extract.rs
@@ -23,6 +23,35 @@ pub fn extract_variables(data: &JsonValue, scope: &Table) -> Result<Table> {
                         let msg = format!("# Got '{}' = '{}'", key, val);
                         eprintln!("{}", truncate(&msg).yellow());
                     }
+                } else if let Value::Table(conf) = value {
+                    let list = Selector::new(&get_string_or_panic(&conf, "list")).unwrap();
+                    let value = Selector::new(&get_string_or_panic(&conf, "value")).unwrap();
+                    let name = Selector::new(&get_string_or_panic(&conf, "name")).unwrap();
+
+                    let res = list.find(&data).next().unwrap();
+                    match res {
+                        JsonValue::Array(items) => {
+                            let x: Vec<Value> = items.iter()
+                                .map(|k| {
+                                    let v = value.find(&k).next().unwrap();
+                                    let n = name.find(&k).next().unwrap();
+
+                                    let mut out = Table::new();
+                                    out.insert("value".to_string(), Value::try_from(v).unwrap());
+                                    out.insert("name".to_string(), Value::try_from(n).unwrap());
+
+                                    Value::Table(out)
+                                })
+                                .collect();
+
+                            let msg = format!("# Got '{}' with {} elements", key, x.len());
+                            eprintln!("{}", truncate(&msg).yellow());
+
+                            let result = Value::Array(x);
+                            out.insert(key.clone(), result);
+                        }
+                        _ => panic!("Oops"),
+                    }
                 }
             }
         },
@@ -33,41 +62,66 @@ pub fn extract_variables(data: &JsonValue, scope: &Table) -> Result<Table> {
     Ok(out)
 }
 
+fn get_string_or_panic(table: &Table, key: &str) -> String {
+    match table.get(key) {
+        Some(Value::String(s)) => s.clone(),
+        _ => panic!("Oops"),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn extracts_variables_from_json() {
-        // Scope should contain an _extract attribute with a jsonpath that reference
-        // an element in data
-        let scope = create_scope();
+        let env = toml::from_str(r#"
+        url = "example.com"
 
-        let data = create_data();
-        let res = extract_variables(&data, &scope).unwrap();
+        [_extract]
+        token = "$.Data.Token"
+        "#).unwrap();
+
+        let data = serde_json::from_str(r#"{ 
+            "Data": { "Token": "kokobaba1234" } 
+        }"#).unwrap();
+
+        let res = extract_variables(&data, &env).unwrap();
 
         assert!(res.get("token").is_some());
         assert_eq!(&Value::String(String::from("kokobaba1234")), res.get("token").unwrap());
     }
 
-    fn create_data() -> JsonValue {
-        let json = r#"{ "Data": { "Token": "kokobaba1234" } }"#;
+    #[test]
+    fn extracts_multiple_values_into_array() {
+        // workaround: Jsonpath crate doesn't support array
+        let env = toml::from_str(r#"
+        url = "example.com"
 
-        return serde_json::from_str(json).unwrap();
-    }
+        [_extract]
+        ToolId = { list = "$.Tools", value = "$.ToolId", name = "$.Name" }
+        "#).unwrap();
 
-    fn create_scope() -> Table {
-        let mut table = Table::new();
+        let data = serde_json::from_str(r#"{ 
+            "Tools": [
+                { "ToolId": 123, "Name": "First tool" },
+                { "ToolId": 345, "Name": "Second tool" }
+            ]
+        }"#).unwrap();
+        let res = extract_variables(&data, &env).unwrap();
 
-        table.insert(String::from("url"), Value::from("example.com"));
+        let expected: Table = toml::from_str(r#"
+            ToolId = [
+                { value = 123, name = "First tool" },
+                { value = 345, name = "Second tool" },
+            ]
+        "#).unwrap();
 
-        let mut extract = Table::new();
-
-        extract.insert(String::from("token"), Value::from("$.Data.Token"));
-
-        table.insert(String::from("_extract"), Value::from(extract));
-
-        table
+        assert!(res.get("ToolId").is_some());
+        assert_eq!(
+            res.get("ToolId").unwrap(),
+            expected.get("ToolId").unwrap(),
+        );
     }
 }
 
