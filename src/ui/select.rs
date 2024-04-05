c2f6b16f9eed2369f401f5eeecd2cd7411523821
Robin Pedersen <robin.pedersen@visma.com>
2024-04-05 16:45:50 +0200

    Merge SelectRequest and Select into one concept
    (Adding fuzzy search to substitution select)

--- src/ui/select.rs
+++ src/ui/select.rs
@@ -1,4 +1,4 @@
-use crossterm::event::{Event, KeyCode, KeyEventKind, KeyModifiers};
+use crossterm::event::{Event, KeyCode, KeyModifiers};
 use fuzzy_matcher::skim::SkimMatcherV2;
 use ratatui::{
     layout::{Constraint, Direction, Layout, Rect},
@@ -18,18 +18,102 @@ pub trait Component {
 
 #[derive(Default)]
 pub struct RequestSelector {
-    search: String,
-    items: Vec<String>,
-    list_state: ListState,
+    selector: Select,
 }
 
 impl RequestSelector {
     pub fn new(reqs: &[String]) -> Self {
+        let items = reqs.into_iter().map(|a| String::from(a)).collect();
+
         Self {
-            search: String::new(),
-            items: reqs.into_iter().map(|a| String::from(a)).collect(),
-            list_state: ListState::default().with_selected(Some(0)),
+            selector: Select::new("Requests".into(), items),
         }
+    }
+
+    pub fn selected_path(&self) -> Option<&String> {
+        self.selector.selected_item()
+    }
+}
+
+impl Component for RequestSelector {
+    fn render_ui(&mut self, frame: &mut Frame, area: Rect) {
+        self.selector.render_ui(frame, area);
+    }
+
+    fn handle_event(&mut self, event: Event) -> bool {
+        self.selector.handle_event(event)
+    }
+}
+
+fn format_item<'a>(text: String, indexes: Vec<usize>) -> Line<'a> {
+    // FIXME: Make '.http' part dark gray
+    // For this, we need to support a configurable "formatter"
+
+    Line::from(
+        text.chars()
+            .enumerate()
+            .map(|(i, c)| {
+                Span::from(String::from(c)).style(if indexes.contains(&i) {
+                    Style::new().yellow()
+                } else {
+                    Style::new().clone()
+                })
+            })
+            .collect::<Vec<_>>(),
+    )
+}
+
+#[derive(Default)]
+pub struct Select {
+    title: String,
+    items: Vec<String>,
+    list_state: ListState,
+    search: String,
+}
+
+impl Select {
+    pub fn new(title: String, items: Vec<String>) -> Self {
+        Self {
+            title,
+            items,
+            list_state: ListState::default().with_selected(Some(0)),
+            search: String::new(),
+        }
+    }
+}
+
+impl Select {
+    pub fn selected(&self) -> Option<usize> {
+        self.list_state.selected()
+    }
+
+    pub fn selected_item(&self) -> Option<&String> {
+        if let Some(i) = self.list_state.selected() {
+            // FIXME: Needs to use index in get_list_items()
+            self.items.get(i)
+        } else {
+            None
+        }
+    }
+
+    pub fn select_next(&mut self) {
+        let len = self.get_list_items().len();
+        match self.list_state.selected() {
+            None => self.list_state.select(Some(0)),
+            Some(i) => self.list_state.select(Some((i + 1) % len)),
+        }
+    }
+
+    pub fn select_prev(&mut self) {
+        let len = self.get_list_items().len();
+        match self.list_state.selected() {
+            None => self.list_state.select(Some(len - 1)),
+            Some(i) => self.list_state.select(Some((len + i - 1) % len)),
+        }
+    }
+
+    pub fn select_first(&mut self) {
+        self.list_state.select(Some(0));
     }
 
     fn get_list_items<'a>(&self) -> Vec<Line<'a>> {
@@ -58,37 +142,12 @@ impl RequestSelector {
                 .collect()
         }
     }
-
-    pub fn select_first(&mut self) {
-        self.list_state.select(Some(0));
-    }
-
-    pub fn select_next(&mut self) {
-        let len = self.get_list_items().len();
-        match self.list_state.selected() {
-            None => self.list_state.select(Some(0)),
-            Some(i) => self.list_state.select(Some((i + 1) % len)),
-        }
-    }
-
-    pub fn select_prev(&mut self) {
-        let len = self.get_list_items().len();
-        match self.list_state.selected() {
-            None => self.list_state.select(Some(len - 1)),
-            Some(i) => self.list_state.select(Some((len + i - 1) % len)),
-        }
-    }
-
-    pub fn selected_path(&self) -> Option<&String> {
-        match self.list_state.selected() {
-            Some(i) => self.items.get(i),
-            None => None,
-        }
-    }
 }
 
-impl Component for RequestSelector {
+impl Component for Select {
     fn render_ui(&mut self, frame: &mut Frame, area: Rect) {
+        frame.render_widget(Clear, area);
+
         let layout = Layout::new(
             Direction::Vertical,
             [Constraint::Min(0), Constraint::Max(3)],
@@ -101,7 +160,7 @@ impl Component for RequestSelector {
             .block(
                 Block::new()
                     .borders(Borders::TOP | Borders::LEFT | Borders::RIGHT)
-                    .title("Requests"),
+                    .title(self.title.clone()),
             )
             .highlight_style(Style::new().reversed())
             .highlight_symbol("> ")
@@ -177,111 +236,6 @@ impl Component for RequestSelector {
             }
         } else {
             false
-        }
-    }
-}
-
-fn format_item<'a>(text: String, indexes: Vec<usize>) -> Line<'a> {
-    // FIXME: Make '.http' part dark gray
-
-    Line::from(
-        text.chars()
-            .enumerate()
-            .map(|(i, c)| {
-                Span::from(String::from(c)).style(if indexes.contains(&i) {
-                    Style::new().yellow()
-                } else {
-                    Style::new().clone()
-                })
-            })
-            .collect::<Vec<_>>(),
-    )
-}
-
-#[derive(Default)]
-pub struct Select {
-    title: String,
-    items: Vec<String>,
-    list_state: ListState,
-}
-
-impl Select {
-    pub fn new(title: String, items: Vec<String>) -> Self {
-        Self {
-            title,
-            items,
-            list_state: ListState::default().with_selected(Some(0)),
-        }
-    }
-}
-
-impl Select {
-    pub fn selected(&self) -> Option<usize> {
-        self.list_state.selected()
-    }
-
-    pub fn select_next(&mut self) {
-        let len = self.items.len();
-        match self.list_state.selected() {
-            None => self.list_state.select(Some(0)),
-            Some(i) => self.list_state.select(Some((i + 1) % len)),
-        }
-    }
-
-    pub fn select_prev(&mut self) {
-        let len = self.items.len();
-        match self.list_state.selected() {
-            None => self.list_state.select(Some(len - 1)),
-            Some(i) => self.list_state.select(Some((len + i - 1) % len)),
-        }
-    }
-}
-
-impl Component for Select {
-    fn render_ui(&mut self, frame: &mut Frame, area: Rect) {
-        frame.render_widget(Clear, area);
-
-        let list_items = self.items.clone();
-
-        let list = List::new(list_items)
-            .highlight_style(Style::new().reversed())
-            .highlight_symbol("> ")
-            .highlight_spacing(HighlightSpacing::Always)
-            .block(Block::bordered().title(self.title.clone()));
-
-        frame.render_stateful_widget(list, area, &mut self.list_state);
-    }
-
-    fn handle_event(&mut self, event: Event) -> bool {
-        match event {
-            Event::Key(key) if key.kind == KeyEventKind::Press => {
-                if key.modifiers.contains(KeyModifiers::CONTROL) {
-                    match key.code {
-                        KeyCode::Char('j') => {
-                            self.select_next();
-                            true
-                        }
-                        KeyCode::Char('k') => {
-                            self.select_prev();
-                            true
-                        }
-                        _ => false,
-                    }
-                } else {
-                    match key.code {
-                        KeyCode::Down => {
-                            self.select_next();
-                            true
-                        }
-                        KeyCode::Up => {
-                            self.select_prev();
-                            true
-                        }
-                        _ => false,
-                    }
-                }
-            }
-            _ => false,
         }
     }
 }
