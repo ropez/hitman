8935a8afa3a118bca7c4c9b467ef44882bbca60c
Robin Pedersen <robin.pedersen@visma.com>
2024-04-05 16:44:31 +0200

    Panic hook, and ensure screen is cleared
    (Screen artifacts when running cargo-watch)

--- src/main_ui.rs
+++ src/main_ui.rs
@@ -1,8 +1,8 @@
-use std::io::stdout;
+use std::{io::stdout, panic::{take_hook, set_hook}};
 
 use anyhow::Result;
 use crossterm::{
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen, Clear, ClearType},
     ExecutableCommand,
 };
 use ratatui::{backend::CrosstermBackend, Terminal};
@@ -13,6 +13,8 @@ mod ui;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    init_panic_hook();
+
     let _guard = Screen::enable()?;
 
     let mut app = App::new()?;
@@ -23,12 +25,22 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
+pub fn init_panic_hook() {
+    let original_hook = take_hook();
+    set_hook(Box::new(move |panic_info| {
+        // intentionally ignore errors here since we're already in a panic
+        let _ = restore_tui();
+        original_hook(panic_info);
+    }));
+}
+
 struct Screen;
 
 impl Screen {
     fn enable() -> Result<Self, std::io::Error> {
         enable_raw_mode()?;
         stdout().execute(EnterAlternateScreen)?;
+        stdout().execute(Clear(ClearType::All))?;
 
         Ok(Screen)
     }
@@ -36,7 +48,12 @@ impl Screen {
 
 impl Drop for Screen {
     fn drop(&mut self) {
-        let _ = stdout().execute(LeaveAlternateScreen);
-        let _ = disable_raw_mode();
+        let _ = restore_tui();
     }
+}
+
+fn restore_tui() -> Result<()> {
+    stdout().execute(LeaveAlternateScreen)?;
+    disable_raw_mode()?;
+    Ok(())
 }
