00a9068b81008c695d1461653528f3f28680cb15
Robin Pedersen <robin.pedersen@visma.com>
2024-04-08 16:58:03 +0200

    Add command to open editor from request list

--- src/main_ui.rs
+++ src/main_ui.rs
@@ -1,26 +1,30 @@
-use std::{io::stdout, panic::{take_hook, set_hook}};
+use std::{
+    io::{self, stdout},
+    panic::{set_hook, take_hook},
+};
 
 use anyhow::Result;
 use crossterm::{
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen, Clear, ClearType},
+    terminal::{
+        disable_raw_mode, enable_raw_mode, EnterAlternateScreen,
+        LeaveAlternateScreen,
+    },
     ExecutableCommand,
 };
 use ratatui::{backend::CrosstermBackend, Terminal};
 
-use ui::app::App;
+use ui::app::{App, Screen};
 
 mod ui;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     init_panic_hook();
-
-    let _guard = Screen::enable()?;
-
     let mut app = App::new()?;
 
+    let screen = CrosstermScreen::default();
     let terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
-    app.run(terminal).await?;
+    app.run(terminal, screen).await?;
 
     Ok(())
 }
@@ -28,32 +32,24 @@ async fn main() -> Result<()> {
 pub fn init_panic_hook() {
     let original_hook = take_hook();
     set_hook(Box::new(move |panic_info| {
-        // intentionally ignore errors here since we're already in a panic
-        let _ = restore_tui();
+        let _ = CrosstermScreen::default().leave();
         original_hook(panic_info);
     }));
 }
 
-struct Screen;
+#[derive(Default)]
+struct CrosstermScreen;
 
-impl Screen {
-    fn enable() -> Result<Self, std::io::Error> {
+impl Screen for CrosstermScreen {
+    fn enter(&self) -> io::Result<()> {
         enable_raw_mode()?;
         stdout().execute(EnterAlternateScreen)?;
-        stdout().execute(Clear(ClearType::All))?;
-
-        Ok(Screen)
+        Ok(())
     }
-}
 
-impl Drop for Screen {
-    fn drop(&mut self) {
-        let _ = restore_tui();
+    fn leave(&self) -> io::Result<()> {
+        stdout().execute(LeaveAlternateScreen)?;
+        disable_raw_mode()?;
+        Ok(())
     }
-}
-
-fn restore_tui() -> Result<()> {
-    stdout().execute(LeaveAlternateScreen)?;
-    disable_raw_mode()?;
-    Ok(())
 }
